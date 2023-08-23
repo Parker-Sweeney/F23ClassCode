46d3091514041f5c9fc4f5f9b10bcd0ba3e9d128
Parker Sweeney <77077612+Parker-Sweeney@users.noreply.github.com>
2023-08-23 11:17:07 -0500

    Update Day_2.py
    Area of Square and Loan Balance Calculation

--- CompThink/Week 1/Day_2.py
+++ CompThink/Week 1/Day_2.py
@@ -1,5 +1,9 @@
 # Area of a square
-
 side = int(input('How long is one side of the square? '))
-
 print('The area is',side * side, 'units')
+
+#Loan calculation
+loan_amt = input(('What is your loan amount? '))
+interest_rate = input('What is your interest rate for this loan (input as a decimal)?')
+balance = float(loan_amt) * float(interest_rate)
+print('Your student loan balance after one year is', balance)
